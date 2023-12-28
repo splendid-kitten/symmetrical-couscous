e40251b9bd2ec6c1b3bfd3aa0fd1e20fad089a12
ArtiKitten <lebrasseur.raph@gmail.com>
2023-12-28 18:32:58 -0500

    Edit the docker-compose and the init script for MongoDB

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Importing data into MongoDB"
 mongoimport --host localhost --db symmetrical-couscous --collection geolocData --type tsv --file /docker-entrypoint-initdb.d/dataset.tsv --headerline
+mongoimport --host localhost --db symmetrical-couscous --collection visitData --type tsv --file /docker-entrypoint-initdb.d/examplevisits.tsv --headerline
