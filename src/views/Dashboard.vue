2cc8dadb3d2c2d41ff645a08fcc04cb74c164abc
ArtiKitten <lebrasseur.raph@gmail.com>
2023-12-28 17:47:15 -0500

    Dashboard cleanup (recent visits)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { onMounted, reactive, ref, watch } from 'vue';
-import ProductService from '@/service/ProductService';
+import { reactive, ref, watch } from 'vue';
 import { useLayout } from '@/layout/composables/layout';
 
 const { isDarkTheme } = useLayout();
 
-const products = ref(null);
 const lineData = reactive({
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
@@ -30,15 +28,8 @@ const lineData = reactive({
 });
 
 const lineOptions = ref(null);
-const productService = new ProductService();
 
-onMounted(() => {
-    productService.getProductsSmall().then((data) => (products.value = data));
-});
 
-const formatCurrency = (value) => {
-    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-};
 
 const applyLightTheme = () => {
     lineOptions.value = {
@@ -199,26 +190,12 @@ export default {
       <div class="col-12 xl:col-6">
           <div class="card">
               <h5>Recent Visits</h5>
-              <DataTable :value="products" :rows="5" :paginator="true" responsiveLayout="scroll">
-                  <Column style="width: 15%">
-                      <template #header> Image </template>
-                      <template #body="slotProps">
-                          <img :src="'demo/images/product/' + slotProps.data.image" :alt="slotProps.data.image" width="50" class="shadow-2" />
-                      </template>
-                  </Column>
-                  <Column field="name" header="Name" :sortable="true" style="width: 35%"></Column>
-                  <Column field="price" header="Price" :sortable="true" style="width: 35%">
-                      <template #body="slotProps">
-                          {{ formatCurrency(slotProps.data.price) }}
-                      </template>
-                  </Column>
-                  <Column style="width: 15%">
-                      <template #header> View </template>
-                      <template #body>
-                          <Button icon="pi pi-search" type="button" class="p-button-text"></Button>
-                      </template>
-                  </Column>
-              </DataTable>
+              <DataTable :value="visits" :rows="5" :paginator="true" responsiveLayout="scroll">
+                  <Column field="ID" header="ID" style="width: 35%"></Column>
+                  <Column field="lat" header="lat" style="width: 35%"></Column>
+                  <Column field="long" header="long" style="width: 35%"></Column>
+                  <Column field="time" header="time" :sortable="true" style="width: 35%"></Column>
+                </DataTable>
           </div>
           
       </div>
