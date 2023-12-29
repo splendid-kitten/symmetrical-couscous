a6d7e2541d784c1a277963466b72752c5f8d1db9
ArtiKitten <lebrasseur.raph@gmail.com>
2023-12-29 10:38:29 -0500

    Generate visits data with a function

--- netlify/functions/generate-visit-data.mjs
+++ netlify/functions/generate-visit-data.mjs
@@ -47,9 +47,10 @@ export const handler = async (event, context) => {
             const currentVisit = currentVisits[data.propulso_id];
             currentVisit.end_time = data.time;
 
-            // Calculate the duration of the visit in seconds by converting the ISO string to a Date object
-            const startTime = new Date(`1970-01-01T${currentVisit.start_time}Z`);
-            const endTime = new Date(`1970-01-01T${data.time}Z`);
+            // Calculate the duration of the visit in seconds by converting the string to a Date object
+
+            const startTime = new Date(`${currentVisit.date}T${currentVisit.start_time}Z`);
+            const endTime = new Date(`${data.date}T${data.time}Z`);
 
             currentVisit.duration = (endTime - startTime) / 1000;
 
@@ -67,9 +68,9 @@ export const handler = async (event, context) => {
         const currentVisit = currentVisits[propulso_id];
         currentVisit.end_time = geolocData[geolocData.length - 1].time; // Existing visit's end time is the last time in the array
 
-        // Calculate the duration of the visit in seconds by converting the ISO string to a Date object
-        const startTime = new Date(`1970-01-01T${currentVisit.start_time}Z`);
-        const endTime = new Date(`1970-01-01T${currentVisit.end_time}Z`);
+        // Calculate the duration of the visit in seconds by converting thestring to a Date object
+        const startTime = new Date(`${currentVisit.date}T${currentVisit.start_time}Z`);
+        const endTime = new Date(`${geolocData[geolocData.length - 1].date}T${currentVisit.end_time}Z`);
         currentVisit.duration = (endTime - startTime) / 1000;
 
         currentVisit.lat /= currentVisit.pingCount;
